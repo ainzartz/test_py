5957c6922d6e1ac0a57bf200f17828a04c4186e8
ainzartz <ainzartz@gmail.com>
2018-10-03 16:54:28 -0400

    commit asynco

---
+++ Asynco/fib.py
@@ -0,0 +1,25 @@
+from time import time
+from functools import wraps
+
+
+def fib(n):
+    return fib(n - 1) + fib(n - 2) if n > 1 else n
+
+
+def log_execution_time(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time()
+        return_value = func(*args, **kwargs)
+        message = "Executing {} took {:.03} seconds.".format(func.__name__,
+                                                             time() - start)
+        print(message)
+        return return_value
+    return wrapper 
+
+def timed_fib(n): 
+    return  log_execution_time(fib(n))
+
+
+
+    
--- thread.py
+++ thread.py
@@ -1,19 +1,45 @@
 from threading import Thread
+from time import sleep
+from time import time
+from functools import wraps
 
-def do_work(start, end, result):
-    sum = 0 
-    for i in range(start, end):
-        sum += i 
-    result.append(sum)
-    return
 
-if __name__ == "__main__":
-    T_START, T_END = 0, 20000000
-    print(type(T_START))
-    print (T_START)
-    result = list() 
-    th1 = Thread(target=do_work, args=（T_START,　T_END, result))
-#    th1.start()
-#    th1.join()
-#print( sum(result) )
 
+def log_execution_time(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time()
+        return_value = func(*args, **kwargs)
+        message = "Executing {} took {:.03} seconds.".format(func.__name__,
+                                                             time() - start)
+        print(message)
+        return return_value
+    return wrapper 
+
+
+
+def fib(n):
+    return fib(n - 1) + fib(n - 2) if n > 1 else n
+    
+
+def print_hello():
+    while True:
+        print("{} - Hello world!".format(int(time())))
+        sleep(3)
+
+def read_and_process_input(n):
+    while True:
+        print('fib({}) = {}'.format(n, log_execution_time(fib(n))))
+
+
+def main():
+    # Second thread will print the hello message. Starting as a daemon means
+    # the thread will not prevent the process from exiting.
+    t = Thread(target=print_hello)
+    t.daemon = True
+    t.start()
+    # Main thread will read and process input
+    read_and_process_input(27)
+
+if __name__ == '__main__':
+    main()
